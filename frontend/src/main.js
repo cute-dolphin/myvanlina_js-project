253fddbba2629c02361559c36762f18ee71e7f84
FHWz5484728 <z5484728@ad.unsw.edu.au.com>
2024-10-18 16:17:42 +1100

    basic login page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,4 +2,28 @@ import { BACKEND_PORT } from './config.js';
 // A helper you may want to use when uploading new images to the server.
 import { fileToDataUrl } from './helpers.js';
 
-console.log('Let\'s go!');
+//functinon parts
+const createLine=(content,id,type)=>{
+    const div1=document.createElement("div");
+    const label=document.createElement("label");
+    const input=document.createElement("input");
+    input.setAttribute("id",id);
+    label.innerText=content;
+    input.type=type;
+    div1.appendChild(label);
+    div1.appendChild(input);
+    return div1;
+}
+
+const createButton=(content,id,type)=>{
+    const button=document.createElement("button");
+    button.innerText=content;
+    button.setAttribute("id",id);
+    button.setAttribute("type",type);
+    return button;
+}
+const main=document.getElementById("main");
+main.appendChild(createLine("Email: ","input-email","email"));
+main.appendChild(createLine("Password: ","input-password","password"));
+main.appendChild(createButton("Submit: ","submit-button","button"));
+
